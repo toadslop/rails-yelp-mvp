e1810838a75094f9f0ff09a4a036781e4877a21f
Brian Heise <bnheise@gmail.com>
2020-02-19 14:00:21 +0900

    seeded the database

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,20 @@
 class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(
+      :name,
+      :address,
+      :phone_number,
+      :category
+    )
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Making some restaurants'
+
+100.times do
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: Restaurant::CATEGORIES.sample
+  )
+  restaurant.save!
+end
+puts '...finished making restaurants'
+
+puts 'Making reviews...'
+Restaurant.find_each do |restaurant|
+  5.times do
+    review = Review.new(
+      content: Faker::Restaurant.review,
+      rating: Review::RATINGS.sample,
+      restaurant_id: restaurant.id
+    )
+    review.save!
+  end
+end
+
+puts '...finished making reviews'
