5acbe0aecdf723d62d3e26ff9d425e0e3122d18f
Brian Heise <bnheise@gmail.com>
2020-02-19 15:31:01 +0900

    completed the user stories, with cancel buttons!

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,24 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [:show]
+
   def index
     @restaurants = Restaurant.all
+  end
+
+  def show
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
   end
 
   private
